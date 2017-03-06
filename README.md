5dc7260fad7796b5b05406dcbdb7eb44e2a34acf
Itai Tenenbaum <taitulism@gmail.com>
2017-03-06 23:24:43 +0200

    init

---
+++ README.md
@@ -0,0 +1,93 @@
+van-go.sh
+=========
+
+A color library for bash (for 256 colors terminals)
+
+Color Numbers
+-------------
+Built-in colors:
+```
+black      = 232
+red        = 160
+green      = 28
+yellow     = 190
+blue       = 27
+steel-blue = 69
+magenta    = 126
+cyan       = 81
+white      = 15
+pink       = 207
+orange     = 202
+purple     = 54
+bordeaux   = 88
+gray       = 246
+light-gray = 253
+```
+
+
+Colored Screen Loggers
+----------------------
+Will print your text in different colors:
+```sh
+logTitle 'MY TITLE'
+logText 'bla bla text'
+logComment 'a comment about stuff'
+logInfo 'info'
+logOK 'ok'
+logWarn 'warn'
+logError 'error'
+logAttention 'attention'
+logFail 'fail'
+logSuccess 'success'
+```
+\*img*
+
+
+Other API
+---------
+###createColor  
+Creates a full color string: `"<flag>;<foreground>;<background>"`
+```sh
+myColor=$(createColor yellow red 1)
+
+echo $myColor
+> "1;38;5;190;48;5;160"
+```
+**Explaination:**  
+; semicolon is the delimiter  
+
+* **1**;38;5;190;48;5;160  
+1 is the **bold** flag.  
+2 is for **dim**  
+4 is for __underline__  
+Combine flags by escaping the semicolon with a backslash or just wrap with single quotes: `1\;4` or `'1;4'`
+
+* 1;38;5;**190**;48;5;**160**    
+190 is **red**, 160 is **yellow**
+
+* 1;**38;5**;190;**48;5**;160  
+38;5; precedes a foreground color  
+48;5; precedes a background color
+
+38;5;190 is yellow foreground  
+48;5;160 is red background
+
+
+
+###colorize  
+Returns your text colorized with your color
+```
+myColor=$(createColor yellow red 1)
+
+echo $(colorize $myColor hello world)
+```
+***NOTE** color flags are passed as the LAST argument*
+
+
+
+###colorz  
+There's a `colorz` assocsiative array (a dictionary) of colors.  
+You can add your own colors by doing: 
+```sh
+colorz[my-color]=130    # 0-255
+```
---
+++ colors.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+function escape () {
+    local escStr=$1
+
+    echo "\e[${escStr}m"
+}
+
+# takes a color name, returns its number
+function getColorNumber () {
+    local colorName=$1
+    local bg=${2:-false}
+
+    if [ "$colorName" = 0 ] ;then
+        return
+    fi
+
+    local colorNumber=${colorz[$colorName]}
+
+    if [ "$bg" = 1 ] ;then
+        echo "48;5;$colorNumber"
+    else
+        echo "38;5;$colorNumber"
+    fi
+}
+
+# params: (fgColor, bgColor, bold=0)
+# return: "<bold>;<fg>;<bg>"
+# usage: `createColor yellow red 1`
+function createColor () {
+    local fg_name=$1
+    local bg_name=${2:-0}
+    local bold=${3:-0}
+
+    local fg_number=$(getColorNumber $fg_name 0)
+    local bg_number=$(getColorNumber $bg_name 1)
+
+    # if exists precede with a semicolon ";" to split 
+    local fgColor=${fg_number:+";$fg_number"}
+    local bgColor=${bg_number:+";$bg_number"}
+
+    # "<bold>;<fg>;<bg>"
+    echo "${bold}${fgColor}${bgColor}"
+}
+
+# params: (createColor, text)
+function colorize () {
+    local color=$(escape $1)
+
+    # shift args to the left (remove first arg)
+    shift
+
+    # "$*" contains all the remaining args (without the first, the color)
+    echo ${color}$*${NO_COLOR}
+}
+
+
+function logInfo () {
+    echo $(colorize $INFO_COLOR $*)
+}
+
+function logTitle () {
+    echo $(colorize $TITLE_COLOR $*)
+}
+
+function logText () {
+    echo $(colorize $TEXT_COLOR $*)
+}
+
+function logComment () {
+    echo $(colorize $COMMENT_COLOR $*)
+}
+
+function logOK () {
+    echo $(colorize $OK_COLOR $*)
+}
+
+function logWarn () {
+    echo $(colorize $WARN_COLOR $*)
+}
+
+function logAttention () {
+    echo $(colorize $HIGHLIGHT_COLOR $*)
+}
+
+function logError () {
+    echo $(colorize $ERROR_COLOR $*)
+}
+
+function logFail () {
+    echo $(colorize $FAIL_COLOR $*)
+}
+
+function logSuccess () {
+    echo $(colorize $SUCCESS_COLOR $*)
+}
+
+
+declare -A colorz
+# color numbers
+colorz[black]=232
+colorz[red]=160
+colorz[green]=28
+colorz[yellow]=190
+colorz[blue]=27
+colorz[steel-blue]=69
+colorz[magenta]=126
+colorz[cyan]=81
+colorz[white]=15
+colorz[pink]=207
+colorz[orange]=202
+colorz[purple]=54
+colorz[bordeaux]=88
+colorz[gray]=246
+colorz[light-gray]=253
+
+NO_COLOR="\e[0m" # Reset Color
+
+# full colors strings "<bold>;<fg>;<bg>"
+INFO_COLOR=$(createColor steel-blue)
+TITLE_COLOR=$(createColor yellow 0 4)
+TEXT_COLOR=$(createColor light-gray)
+COMMENT_COLOR=$(createColor gray)
+OK_COLOR=$(createColor green 0 1)
+WARN_COLOR=$(createColor orange 0 1)
+HIGHLIGHT_COLOR=$(createColor yellow 0 1)
+ERROR_COLOR=$(createColor red 0 1)
+FAIL_COLOR=$(createColor yellow red 1)
+SUCCESS_COLOR=$(createColor white green 1)
+
+
+echo $FAIL_COLOR
